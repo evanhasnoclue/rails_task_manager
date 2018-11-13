c4c6388b2b1d89963fc1338f6cf0cc56fc5e8cd8
kai wang <wangkai199385@126.com>
2018-11-13 13:39:37 +0800

    finish the basis of task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,50 @@
 class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def show
+    if @task.completed
+      @message = "This task is completed!"
+    else
+      @message = "This task is not completed yet!"
+    end
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task.id)
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get all tasks
-  get 'tasks', to: 'tasks#index', as: :tasks
+  # # get all tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
 
-  # show specific task  tasks/1
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # add a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # add a new task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # show specific task  tasks/1
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # edit a task  tasks/1/edit
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # edit a task  tasks/1/edit
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # delete one task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # delete one task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
+  resources :tasks
+
+  root to: 'tasks#index'
 end
