b5f04947cc652a72d48fa47e8d1d25cfa0f6cdef
kai wang <wangkai199385@126.com>
2018-11-13 11:32:43 +0800

    set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  # show specific task  tasks/1
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # add a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # edit a task  tasks/1/edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete one task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
